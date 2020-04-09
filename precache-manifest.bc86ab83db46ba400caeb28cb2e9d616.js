8e08b139c47e54135d36f8797005568758b58052
HARSH JAIN <jainharsh270@gmail.com>
2020-04-09 12:27:03 +0530

    Updates

---
+++ precache-manifest.bc86ab83db46ba400caeb28cb2e9d616.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7797e2ba9c450bf6390236adb365b3c6",
+    "url": "/Flip-IT/index.html"
+  },
+  {
+    "revision": "ac74ff8b0dd0663b5d87",
+    "url": "/Flip-IT/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "01a6bfa2ae7284624d8f",
+    "url": "/Flip-IT/static/js/2.c86959d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Flip-IT/static/js/2.c86959d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac74ff8b0dd0663b5d87",
+    "url": "/Flip-IT/static/js/main.82f886d9.chunk.js"
+  },
+  {
+    "revision": "e3a6226532eae60c3238",
+    "url": "/Flip-IT/static/js/runtime-main.8d058dd2.js"
+  }
+]);
