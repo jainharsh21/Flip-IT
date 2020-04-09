b82900156454fecc21cd8ff528213c8e1e3214d4
HARSH JAIN <jainharsh270@gmail.com>
2020-04-09 12:17:21 +0530

    created the coin flipper

--- src/CoinFlipper.js
+++ src/CoinFlipper.js
@@ -4,12 +4,28 @@ import Coin from "./Coin";
 class CoinFlipper extends Component {
   constructor(props) {
     super(props);
+    this.types = ["heads", "tails"];
     this.state = {
       flips: 0,
       heads: 0,
       tails: 0,
       urlType: "",
     };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  flipCoin() {
+    let randInd = Math.floor(Math.random() * 2);
+    this.setState((curState) => ({
+      flips: curState.flips + 1,
+      heads: randInd === 0 ? curState.heads + 1 : curState.heads,
+      tails: randInd === 0 ? curState.tails : curState.tails + 1,
+      urlType: this.types[randInd],
+    }));
+  }
+
+  handleClick() {
+    this.flipCoin();
   }
 
   render() {
@@ -21,6 +37,7 @@ class CoinFlipper extends Component {
           tails={this.state.tails}
           imgUrl={`https://tinyurl.com/react-coin-${this.state.urlType}-jpg`}
         />
+        <button onClick={this.handleClick}>FLip Coin!</button>
       </div>
     );
   }
